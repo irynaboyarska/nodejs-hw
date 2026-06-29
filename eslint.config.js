45ea464d81be39a77d3a52c692f0debe19de0832
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-06-30 00:00:25 +0300

    add routes and middlewares

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
     rules: {
-      semi: "error",
-      "no-unused-vars": ["error", { args: "none" }],
-      "no-undef": "error",
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
     },
   },
 ]);
--- src/server.js
+++ src/server.js
@@ -1,1 +1,70 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import pino from 'pino-http';
 
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware для парсингу JSON
+app.use(express.json());
+
+// Middleware дозволяє робити запити з інших доменів
+app.use(cors());
+
+// Логування запитів
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// маршрут, який буде повертати всі нотатки
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+// маршрут, який буде повертати одну нотатку за її ідентифікатором
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+// маршрут для імітації виникнення помилки
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware для неіснуючих маршрутів
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+// Middleware для обробки помилок
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
