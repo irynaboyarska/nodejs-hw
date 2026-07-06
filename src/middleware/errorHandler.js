112eca69011a071f42a53f2aa4cbbe8f2fd2c8f4
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-07-06 22:45:52 +0300

    fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,13 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.message);
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
   res.status(500).json({
-    message: 'Internal Server Error',
+    message: err.message || 'Internal Server Error',
   });
 };
