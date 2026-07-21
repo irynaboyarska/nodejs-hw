2bd5bab7f160bbcb5fcf8d44c88c0f62f0c5870f
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-07-21 14:33:34 +0300

    implement user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,12 +2,14 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 import { createSession, setSessionCookies } from '../services/auth.js';
+import { Session } from '../models/session.js';
 
+// user register
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const exitingUser = await User.findOne({ email });
-  if (exitingUser) {
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
     throw createHttpError(400, 'Email in use');
   }
 
@@ -22,4 +24,25 @@ export const registerUser = async (req, res) => {
   setSessionCookies(res, newSession);
 
   res.status(201).json(newUser);
+};
+
+// user login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
 };
