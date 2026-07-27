b73d9bea134a4b6c0a0a75b3da8a65daf952ec15
Iryna Boiarska <boyarskairyna32@gmail.com>
2026-07-27 16:41:23 +0300

    fix

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -7,7 +7,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-  from: process.env.SMTP_FROM,
 });
 
 export const sendEmail = async (options) => {
